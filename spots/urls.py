06ccb127d88b8eb26043b760692081609afd8f87
Danny <superhotfire447@yandex.ru>
2020-11-03 18:16:59 +0300

    add couple views for main page

--- spots/urls.py
+++ spots/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.show_page),
+    path('spotdetails/<int:pk>/', views.fetch_spot_details, name='details'),
     path('cms/<int:pk>', views.show_cms),
 ]
--- spots/views.py
+++ spots/views.py
@@ -1,11 +1,59 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
+from django.urls import reverse
 
 from spots.models import Spot
+
+
 def show_page(request):
-    context = {}
+    """
+    fetch spots geo data
+    """
+    spots = Spot.objects.all()
+    features = []
+    for spot in spots:
+        coordinates = [spot.longitude, spot.latitude]
+        title = spot.title
+        place_id = spot.pk
+
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": coordinates,
+            },
+            "properties": {
+                "title": title,
+                "placeId": place_id,
+                "detailsUrl": reverse('details', kwargs={'pk': place_id})
+            },
+        }
+        features.append(feature)
+
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+    context = {"places_geojson": places_geojson}
     return render(request, 'index.html', context)
+
+
+def fetch_spot_details(request, pk):
+    """
+    Fetch json with spot details
+    """
+    spot = get_object_or_404(Spot, pk=pk)
+    imgs = []
+    coords = {"lat": spot.latitude, "lng": spot.longitude}
+    details = {
+        "title": spot.title,
+        "imgs": imgs,
+        "description_short": spot.description,
+        "description_long": spot.features,
+        "coordinates": coords,
+    }
+    return JsonResponse(details, safe=False, json_dumps_params={'ensure_ascii': False})
 
 
 def show_cms(request, pk):
